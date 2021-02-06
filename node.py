8f4c2acd41b1f4d8b6ddcdc1126837debd50ab00
emilyxzhou <zhouxemily@gmail.com>
2021-02-05 16:51:50 -0800

    Refactor Node class
    Signed-off-by: emilyxzhou <zhouxemily@gmail.com>

--- node.py
+++ node.py
@@ -1,9 +1,24 @@
 class Node:  # each interaction round with user
-    def __init__(self, question, number_of_options, array_of_options, key):
-        self.question = question
-        self.number_of_options = number_of_options
-        self.key = key
-        array_of_options = array_of_options
+    
+    def __init__(
+            self,
+            question,
+            array_of_options,
+            key
+    ):
 
-        def get_next_node_key(self, input_choice_index):
-            return self.array_of_options[input_choice_index].next_node_key
+        self._question = question
+        self._key = key
+        self._array_of_options = array_of_options
+
+    def get_next_node_key(self, input_choice_index):
+        return self.array_of_options[input_choice_index].next_node_key
+
+    @property
+    def array_of_options(self):
+        return self._array_of_options
+
+
+if __name__ == "__main__":
+    node = Node("hello", ["Hi", "Hello"], "greeting")
+    print(len(node.array_of_options))
